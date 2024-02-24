5edf9d0515d9566b49c07d8f7f7567d23092b7be
Daniel Verduras Gallego <daniel.verduras21@gmail.com>
2024-02-24 01:56:21 +0100

    Unit test no local reproducible

--- src/test/java/repository/ModeloDatosTest.java
+++ src/test/java/repository/ModeloDatosTest.java
@@ -7,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
-class ModeloDatosTest {
+public class ModeloDatosTest {
 
     @Test
     void testExisteJugador() {
